b96c9d0db9d4ea59598bbd50cf5db6483213b180
baemsu <83949128+baemsu@users.noreply.github.com>
2022-02-22 17:18:52 +0900

    Update app.py

--- app.py
+++ app.py
@@ -108,6 +108,20 @@ def photo():
 
 def video():
     st.title('캠코더입력')
+    img_file_buffer = st.camera_input("Take a picture")
+
+    if img_file_buffer is not None:
+        # To read image file buffer with OpenCV:
+        bytes_data = img_file_buffer.getvalue()
+        cv2_img = cv2.imdecode(np.frombuffer(bytes_data, np.uint8), cv2.IMREAD_COLOR)
+
+        # Check the type of cv2_img:
+        # Should output: <class 'numpy.ndarray'>
+        st.write(type(cv2_img))
+
+        # Check the shape of cv2_img:
+        # Should output shape: (height, width, channels)
+        st.write(cv2_img.shape)
 
 
 selected_box = st.sidebar.selectbox('다음중 선택해주세요',('설명서','사진파일입력', '캠코더입력'))
